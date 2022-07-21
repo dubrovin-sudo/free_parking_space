e9d00a7c0d05b338168d12c7860ac76c3acdd639
Aleksandr Dubrovin <dubrovinmail@gmail.com>
2022-07-21 03:03:44 +0300

    Cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A way to find that free parking space',
+    author='Aleksandr Dubrovin',
+    license='MIT',
+)
